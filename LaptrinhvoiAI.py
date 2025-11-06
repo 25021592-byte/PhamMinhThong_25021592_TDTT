d80faff2e186e90fab7e50f76983ef1f50db5ffd
25021592-byte <25021592@vnu.edu.vn>
2025-11-06 15:48:50 +0700

    thay doi file

--- LaptrinhvoiAI.py
+++ LaptrinhvoiAI.py
@@ -6,4 +6,24 @@ print(c)
 city="Hà Nội"
 year=2025
 print("Thành phố:",city,"-Năm:",year)
-print("huhu")
+n=4
+t=0
+for i in range(1,n+1):
+    t += i
+    print(t)
+numbers = [1, 2, 3, 4] 
+for x in numbers: 
+    if x % 2 == 0: 
+        print(x, "là số chẵn") 
+    else: 
+        print(x, "là số lẻ") 
+animals = ["cat", "dog", "cat", "bird"] 
+count = 0 
+for a in animals: 
+    count += 1 
+    print("Số phần tử trong danh sách là:", count)
+num = int(input("Nhập số: "))
+if num % 2 == 0:
+    print("Số chẵn")
+else:
+    print("Số lẻ")
